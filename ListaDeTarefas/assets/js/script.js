b76f42d888c148bdba12114ca761ce3b0a51d471
Perkles <isaiasbatistta@gmail.com>
2023-11-09 10:09:22 -0300

    implementations finalized
    added local storage and added informations about tasks is empty or not.

--- ListaDeTarefas/assets/js/script.js
+++ ListaDeTarefas/assets/js/script.js
@@ -14,19 +14,27 @@ const buttonAdd = document.querySelector("#buttonadd")
 const inputEdit = document.querySelector("#inputedit")
 const buttonEdit = document.querySelector("#buttonedit")
 const lists = document.querySelector("#lists")
+const allTasks = document.querySelector("#allTasks")
+const toDo = document.querySelector("#toDo")
+const taskDone = document.querySelector("#taskDone")
 let position
 
 buttonAdd.addEventListener('click', () =>{
     const tarefa = new Tarefa(inputAdd.value)
-    tarefas.push(tarefa)
+    if(tarefa.description){
+        tarefas.push(tarefa)
+    }else{
+        alert("Tarefa Vazia!")
+        location.reload()
+    }
     toShow()
 })
 inputAdd.addEventListener('keydown', (e) =>{
     if(e.key === "Enter"){
         const tarefa = new Tarefa(inputAdd.value)
         tarefas.push(tarefa)
-        toShow()
         inputAdd.value = ''
+        toShow()
         e.preventDefault()
 }})
 const toShow = () => {
@@ -35,13 +43,14 @@ const toShow = () => {
         const li = document.createElement("li")
         li.setAttribute("class",`d-flex flex-row bd-highlight mb-3 border border-5 rounded-4`)
         li.innerHTML =`
-            <button onclick="alterButton(${tarefas.indexOf(e)})" class="btn btn-outline-secondary"><i class="fa-solid fa-check"></i></button>
+            <button onclick="alterButton(${tarefas.indexOf(e)})" class="btn houver"><i class="fa-solid fa-check"></i></button>
             <p class="task ${e.state && "done"}">${e.description}</p>
-            <button class="btn btn-outline-secondary"><i class="fa-solid fa-pen" onclick="editList(${tarefas.indexOf(e)})"></i></button>
-            <button class="btn btn-outline-secondary ms-1"><i class="fa-solid fa-xmark" onclick="inputDelete(${tarefas.indexOf(e)})"></i><button>
+            <button class="btn houver"><i class="fa-solid fa-pen" onclick="editList(${tarefas.indexOf(e)})"></i></button>
+            <button class="btn houver"><i class="fa-solid fa-xmark" onclick="inputDelete(${tarefas.indexOf(e)})"></i></button>
             ` 
         lists.appendChild(li) 
     })
+    localStorage.setItem('lista',JSON.stringify(tarefas))
 }
 function inputDelete(i){
     tarefas.splice(i, 1)
@@ -67,18 +76,69 @@ buttonEdit.addEventListener('click', () =>{
 buttonEdit.addEventListener('keydown', (e) =>{
     if(e.key === "Enter"){
         tarefas[position].setList(inputEdit.value)
-        toShow()
-        inputAdd.value = ''
-        toggleForms()
-        e.preventDefault()
-}})
+        inputEdit.value = ''
+        e.preventDefault() 
+    }
+    toggleForms()
+    toShow()
+})
 
 function alterButton(i){
   tarefas[i].state = !tarefas[i].state
   toShow()
 }
-/*falta inserir o filtro
+allTasks.addEventListener('click', () => {
+    toShow()
+})
+taskDone.addEventListener('click', () =>{
+    let aFazer = tarefas.filter((el) =>{
+       if(el.state == true){
+        return el
+       }
+    })
+    lists.innerHTML = ""
+    aFazer.map((e) =>{ 
+        const li = document.createElement("li")
+        li.setAttribute("class",`d-flex flex-row bd-highlight mb-3 border border-5 rounded-4`)
+        li.innerHTML =`
+            <button onclick="alterButton(${aFazer.indexOf(e)})" class="btn houver"><i class="fa-solid fa-check"></i></button>
+            <p class="task ${e.state && "done"}">${e.description}</p>
+            <button class="btn houver"><i class="fa-solid fa-pen" onclick="editList(${aFazer.indexOf(e)})"></i></button>
+            <button class="btn houver"><i class="fa-solid fa-xmark" onclick="inputDelete(${aFazer.indexOf(e)})"></i></button>
+            ` 
+        lists.appendChild(li) 
+    })
+})
+toDo.addEventListener('click', () =>{
+    let aFazer = tarefas.filter((el) =>{
+       if(el.state == false){
+        return el
+       }
+    })
+    lists.innerHTML = ""
+    aFazer.map((e) =>{ 
+        const li = document.createElement("li")
+        li.setAttribute("class",`d-flex flex-row bd-highlight mb-3 border border-5 rounded-4`)
+        li.innerHTML =`
+            <button onclick="alterButton(${aFazer.indexOf(e)})" class="btn houver"><i class="fa-solid fa-check"></i></button>
+            <p class="task ${e.state && "done"}">${e.description}</p>
+            <button class="btn houver"><i class="fa-solid fa-pen" onclick="editList(${aFazer.indexOf(e)})"></i></button>
+            <button class="btn houver"><i class="fa-solid fa-xmark" onclick="inputDelete(${aFazer.indexOf(e)})"></i></button>
+            ` 
+        lists.appendChild(li) 
+    })
+})
+function reload() {
+    const taskOfLocalStorage = localStorage.getItem('lista');
+    if(taskOfLocalStorage){
+    tarefas = JSON.parse(taskOfLocalStorage);
+    }
+    toShow();
+    console.log(taskOfLocalStorage)
+}
+reload();
+/*
 corrigir bug do editar com evento de teclado
-avisar tarefa vazia
-tarefa repetida
-ficar atento ao botão excluir*/
+
+
+*/
